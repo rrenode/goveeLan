2cd291209920f143c900b8fb694d6af029bb45b6
rrenode <robert.renodej@gmail.com>
2026-03-16 18:43:45 -0400

    explcit compileTime only pragma for & op on arrays

--- src/goveeLan/core/arrayutils.nim
+++ src/goveeLan/core/arrayutils.nim
@@ -1,6 +1,6 @@
 import macros
 
-proc `&`*[T; al, bl: static[int]](a: array[al, T], b: array[bl, T]): array[al + bl, T] =
+proc `&`*[T; al, bl: static[int]](a: array[al, T], b: array[bl, T]): array[al + bl, T] {.compileTime.} =
   for i, e in a: result[i] = e
   for i, e in b: result[i + al] = e
 
